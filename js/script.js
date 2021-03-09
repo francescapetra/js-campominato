a8c6d618846d38fef9dba8f976c37006f267c793
francescapetra <francescalaurapetra@gmail.com>
2021-03-09 19:12:00 +0100

    function + bonus

--- js/script.js
+++ js/script.js
@@ -1,17 +1,38 @@
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
 var numeriEstratti = [];//array vuoto
 var numeroSostituto;
+var richiestaLivello;
+
+
+do {
+  richiestaLivello = parseInt(prompt("scegli il livello: soft = 0, medio = 1, hard = 2"));
+
+  if (richiestaLivello === 0) {
+    richiestaLivello = 100;
+  }else if (richiestaLivello === 1) {
+    richiestaLivello = 80;
+  } else if (richiestaLivello === 2) {
+    richiestaLivello = 50;
+  }
+
+} while (richiestaLivello!= 100 && richiestaLivello!= 80 && richiestaLivello!= 50);
+
+
+randomGeneratore (1,richiestaLivello);
+
+function randomGeneratore(min, richiestaLivello){
 for (var i = 0; i < 16; i++) {
-  var numeriCpu = Math.floor(Math.random() * 100) + 1;
+  var numeriCpu = Math.floor(Math.random() * richiestaLivello) + 1;
   // I numeri non possono essere duplicati.
   if (!numeriEstratti.includes(numeriCpu)){
       numeriEstratti.push(numeriCpu);
     }else {
-      numeroSostituto = Math.floor(Math.random() * 100) + 1;
+      numeroSostituto = Math.floor(Math.random() * richiestaLivello) + 1;
       while (!numeriEstratti.includes(numeriCpu)) {
       }
       numeriEstratti.push(numeroSostituto);
     }
+}
 }
 console.log(numeriEstratti);
 
@@ -24,33 +45,35 @@ var numeriScartati = [];
 var nuovoNumeroUtente;
 var numeroVolte = 0;
 
+inserimentoNumUtente (1,richiestaLivello);
+
+function inserimentoNumUtente (min,richiestaLivello){
 do {
   var numeroUtente = parseInt(prompt("inserire numero"));
 
   if (!arrayUtente.includes(numeroUtente)) {
-    if ((numeroUtente >=1) && (numeroUtente <=100)) {
+    if ((numeroUtente >=min) && (numeroUtente <=richiestaLivello)) {
           arrayUtente.push(numeroUtente);
           numeroVolte+= 1;
 
       }else {
-        alert("il numero deve essere compreso tra 1 e 100");
+        alert("il numero deve essere compreso tra" + min + "e" + richiestaLivello);
       }
     }else {
       alert("il numero è già stato inserito, inseriscine uno nuovo");
   }
-  
+
 // La partita termina quando il giocatore inserisce un numero “vietato”
 // o raggiunge il numero massimo possibile di numeri consentiti.
 
-} while (!numeriEstratti.includes(numeroUtente) && numeroVolte < 84);
+} while (!numeriEstratti.includes(numeroUtente) && numeroVolte < (richiestaLivello-16));
 // Al termine della partita il software deve comunicare il punteggio,
 // cioè il numero di volte che l’utente ha inserito un numero consentito.
   console.log (arrayUtente);
-  console.log (numeroUtente + " il numero esiste, hai perso");
+  console.log (numeroUtente + " il numero è ILLEGAL, hai perso");
   console.log ("totale numeri inseriti: " + numeroVolte);
-  console.log("tentativi rimasti: " + (84 - numeroVolte));
-
-
+  console.log("tentativi rimasti: " + (richiestaLivello- 16 - numeroVolte));
+}
 
 
 // BONUS: (da fare solo se funziona tutto il resto)
