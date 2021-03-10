7ed32c66548fc55e7d0369276799b1f8a3a46571
francescapetra <francescalaurapetra@gmail.com>
2021-03-10 14:56:57 +0100

    curata indentazione e console.log

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@
 var richiestaLivello;
 var min = 1;
 do {
-  richiestaLivello = parseInt(prompt("scegli il livello : soft = 0, medio = 1, hard = 2"));
+  richiestaLivello = parseInt(prompt("Scegli il livello : soft = 0, medio = 1, hard = 2"));
 
   if (richiestaLivello === 0) {
     richiestaLivello = 100;
@@ -19,85 +19,65 @@ do {
 } while (richiestaLivello!= 100 && richiestaLivello!= 80 && richiestaLivello!= 50);
 
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
-var arrayEstratti = randomGeneratore (min,richiestaLivello);
 // rifatta con ciclo while
 // finchè i numeri non sono diversi da essi stessi
+var arrayEstratti = randomGeneratore (min,richiestaLivello);
+
 function randomGeneratore(min, richiestaLivello){
 
   var numeriEstratti = [];//array vuoto metti dentro se no non funzia
   var numeroSostituto;
 
-while (numeriEstratti.length < 16) {
- var numeriCpu = Math.floor(Math.random() * richiestaLivello) + 1;
+  while (numeriEstratti.length < 16) {
+    var numeriCpu = Math.floor(Math.random() * richiestaLivello) + 1;
 
- if (numeriEstratti.includes(numeriCpu) == false){
-  numeriEstratti.push(numeriCpu);
+     if (numeriEstratti.includes(numeriCpu) == false){
+      numeriEstratti.push(numeriCpu);
+      }
+  }
+  return numeriEstratti;
 }
-}
-return numeriEstratti;
-}
-console.log(arrayEstratti);
+console.log("Numeri BOMBA: " + arrayEstratti);
 
 // In seguito deve chiedere all’utente (84) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
 // L’utente non può inserire più volte lo stesso numero.
 // Se il numero è presente nella lista dei numeri generati, la partita termina,
 // altrimenti si continua chiedendo all’utente un altro numero.
 var arrayUtente = [];
-
 var numeroInserito = inserimentoNumUtente (min,richiestaLivello);
 
-
 function inserimentoNumUtente (min,richiestaLivello){
-
 
   var nuovoNumeroUtente;
   var numeroVolte = 0;
 
-do {
-  numeroUtente = parseInt(prompt("inserire numero"));
+  do {
+    numeroUtente = parseInt(prompt("Inserire numero"));
 
-  if (!arrayUtente.includes(numeroUtente)) {
-    if ((numeroUtente >=min) && (numeroUtente <=richiestaLivello)) {
-          arrayUtente.push(numeroUtente);
-          numeroVolte+= 1;
-
+    if (!arrayUtente.includes(numeroUtente)) {
+      if ((numeroUtente >=min) && (numeroUtente <=richiestaLivello)) {
+            arrayUtente.push(numeroUtente);
+            numeroVolte+= 1;
+        }else {
+          alert("Il numero deve essere compreso tra " + min + " e " + richiestaLivello);
+        }
       }else {
-        alert("il numero deve essere compreso tra" + min + "e" + richiestaLivello);
+        alert("Il numero è già stato inserito, inseriscine uno nuovo");
       }
-    }else {
-      alert("il numero è già stato inserito, inseriscine uno nuovo");
-  }
-// La partita termina quando il giocatore inserisce un numero “vietato”
-// o raggiunge il numero massimo possibile di numeri consentiti.
-
-} while (!arrayEstratti.includes(numeroUtente) && numeroVolte < (richiestaLivello-16));
-return [numeroUtente, numeroVolte];
+  // La partita termina quando il giocatore inserisce un numero “vietato”
+  // o raggiunge il numero massimo possibile di numeri consentiti.
+  } while (!arrayEstratti.includes(numeroUtente) && numeroVolte < (richiestaLivello-16));
+  return [numeroUtente, numeroVolte];
 }
-
-
-
-
+// condizione slegata
+// Al termine della partita il software deve comunicare il punteggio,
+// cioè il numero di volte che l’utente ha inserito un numero consentito.
 if ((richiestaLivello- 16 - numeroInserito[1]) == 0) {
 console.log ("HAI VINTO!")
 }else {
-  console.log (numeroInserito[0] + " il numero è BOMBA, hai perso");
+  console.log ("ATTENZIONE il numero " + numeroInserito[0] + " è un numero BOMBA, hai perso.");
 }
-// Al termine della partita il software deve comunicare il punteggio,
-// cioè il numero di volte che l’utente ha inserito un numero consentito.
-  console.log (arrayUtente);
-  console.log ("totale numeri inseriti: " + numeroInserito[1]);
-  console.log ("totale punti : " + (numeroInserito[1] - 1));
-  console.log("inserimenti mancanti alla vittoria: " + (richiestaLivello- 16 - numeroInserito[1]));
-  
-  // for (var i = 0; i < 16; i++) {
-  //   var numeriCpu = Math.floor(Math.random() * richiestaLivello) + 1;
-  //   // I numeri non possono essere duplicati.
-  //   if (!numeriEstratti.includes(numeriCpu)){
-  //       numeriEstratti.push(numeriCpu);
-  //     }else {
-  //       numeroSostituto = Math.floor(Math.random() * richiestaLivello) + 1;
-  //       while (!numeriEstratti.includes(numeriCpu)) {
-  //       }
-  //       numeriEstratti.push(numeroSostituto);
-  //     }
-  // }
+  console.log ("Totale numeri inseriti: " + numeroInserito[1] );
+  console.log ("Numeri giocati : " + arrayUtente);
+  console.log ("Totale punti : " + (numeroInserito[1] - 1));
+  console.log("Punti mancanti alla vittoria: " + (richiestaLivello- 16 - numeroInserito[1] + 1));
